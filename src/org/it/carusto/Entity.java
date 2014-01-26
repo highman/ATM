567aeab478ff7254ccdf4304c24390f226db2d10
Kostya <highman@ua.fm>
2014-01-26 12:39:43 +0200

    third commit

--- src/org/it/carusto/Entity.java
+++ src/org/it/carusto/Entity.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -13,58 +12,70 @@ import java.util.TreeMap;
 
 public class Entity {	
 	private int total;	
-	Map<Integer, Integer> map;
+	private Map<Integer, Integer> currMap = null;
+	private Map<Integer, Integer> tmpMap = null; 
 	
 	public Entity() {		
-		map = new TreeMap<>();
+		currMap = new TreeMap<>();
 		total = 0;
 	}
 				
 	public void addNote(int value, int number) {		
-		if(map.containsKey(value)) {			
-			int nNum = map.get(value) + number;				
-			map.put(value, nNum);			
+		if(currMap.containsKey(value)) {			
+			int nNum = currMap.get(value) + number;				
+			currMap.put(value, nNum);			
 		} else {
-			map.put(value, new Integer(number));
+			currMap.put(value, new Integer(number));
 		}
 		total += (value*number);
 	}
 	
 	public Set<Entry<Integer,  Integer>> getEntrySet () {
-		return  map.entrySet();				
+		return  currMap.entrySet();				
 	}
 	
-	public void getCash(int amount) throws IOException {
-		System.out.println("welcome to getCash ");
+	public void getCash(int amount) throws IOException {		
 		if(amount > total) 
 			throw new IOException();
 		else {	
-			int currValue, needNum, currNum, rest;
-			System.out.println("amount less total ");
-			List<Integer> list = new ArrayList<Integer>(map.keySet());
-			Collections.reverse(list);
-			Iterator<Integer> litr = list.listIterator();
+			int currValue, // Номинал купюры 
+				currNum,   // Текущее наличие купюр выбранного номинала 
+				needNum,   // Необходимое количество купюр выбранного номинала для выдачи				 
+				rest;      // Остаток для выдачи в других купюрах
 			
-			while(litr.hasNext()) {
-				System.out.println("welcome to while ");
+			List<Integer> list = new ArrayList<Integer>(currMap.keySet()); // Получаем перечень всех доступных номиналов
+			Collections.reverse(list); 								   // Сортируем список в обратном порядке от большего к меньшему
+			Iterator<Integer> litr = list.listIterator();
+			tmpMap = new TreeMap<>(currMap);
+			
+			while(litr.hasNext()) {				
 				currValue = (Integer) litr.next();
 				rest = amount % currValue;
-				if(rest == 0) {
-					needNum = amount / currValue;
-					currNum = map.get(currValue);
-					if(needNum == currNum) {
-						map.put(currValue, 0);
-						break;
-					} else if(needNum < currNum) {
-						map.put(currValue, currNum-needNum);
-						break;												
-					} else if(needNum > currNum) {
-						amount -= (currValue*currNum);
-						System.out.println("amount = " + amount);
-					}
-				} else if(!litr.hasNext()) { 
-					throw new IOException();
-				} 				 				
+				amount -= rest;
+				needNum = amount / currValue;
+				currNum = tmpMap.get(currValue);									
+					
+				if(needNum == currNum) {
+					tmpMap.put(currValue, 0);
+					amount -= (currValue*currNum);						
+											
+				} else if(needNum < currNum) {
+					tmpMap.put(currValue, currNum-needNum);
+					amount -= (currValue*needNum);																													
+				
+				} else if(needNum > currNum) {
+					tmpMap.put(currValue, 0);
+					amount -= (currValue*currNum);						 						
+				
+				} 				
+				if(rest != 0) amount +=rest;
+				
+				if(!litr.hasNext()) {
+					if(amount !=0) 
+						throw new IOException();
+					else
+						currMap = tmpMap;
+				}				
 			}				
 		}		
 	}	
